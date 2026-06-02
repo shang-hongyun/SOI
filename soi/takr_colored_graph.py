a0e2311e89f5b660cb63f4d53b9355d7a029ef85
zhangrengang <zhangrenang@ori-gene.cn>
2026-06-02 13:40:24 +0800

    block node attrs: just copy first HOG's full dict

--- soi/takr_colored_graph.py
+++ soi/takr_colored_graph.py
@@ -1533,19 +1533,11 @@ class ColoredGraph(nx.DiGraph):
         block_cg = type(self)(hog_level=self.hog_level)
         for bid in self._blocks:
             block_cg.add_node(bid)
-            # 全量继承 HOG 节点属性（sources/telomere 做并集）
+            # 全量继承第一个 HOG 的节点属性
             hogs = self._blocks[bid]
             first = next((h for h in hogs if self.has_node(h)), None)
             if first:
                 block_cg.nodes[bid].update(self.nodes[first])
-                src_union = set(block_cg.nodes[bid].get('sources', set()))
-                tel_union = set(block_cg.nodes[bid].get('telomere', set()))
-                for h in hogs[1:]:
-                    if self.has_node(h):
-                        src_union.update(self.nodes[h].get('sources', set()))
-                        tel_union.update(self.nodes[h].get('telomere', set()))
-                block_cg.nodes[bid]['sources'] = src_union
-                block_cg.nodes[bid]['telomere'] = tel_union
 
         for h1, h2, data in self.edges(data=True):
             b1 = self._hog_to_block.get(h1)
